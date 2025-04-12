0b4e174acdbd3b848212e38faae3818dd59ac3f5
Angelloescobar <aescobarg21@miumg.edu.gt>
2025-04-11 22:54:44 -0600

    Crud y Forms

---
+++ Program y crud.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SqlServer.Server;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace miFormularioTarea
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
